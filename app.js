fe0cd1fa2a77399e46ca6e54c1a963452620b87e
Teza Alfian <tezaalfian2916@gmail.com>
2023-08-08 09:06:42 +0700

    allow cors origin

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ import { connectDB } from "./src/config/database.js";
 import { logger } from "./src/utils/logging.js";
 import router from "./src/routes/router.js";
 import errorMiddleware from "./src/middleware/error-middleware.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
